399d4d1d2b76587a761d5b47107e9dc715647c43
DianaAndreeaSimionca <33803484+DianaAndreeaSimionca@users.noreply.github.com>
2017-11-19 22:04:40 +0200

    Commit

--- index.php
+++ index.php
@@ -1,33 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
-	<title>Libraria mea</title>
-</head>
-<body>
+<?php
+include ("conectare.php");
+include ("page_top.php");
+include ("meniu.php");
+?>
 
-	<h1>Libraria mea</h1>
+    <td valign="top">
+    <h1>Prima pagina</h1>
+    <b>Cele mai noi carti</b>
+    <table cellpadding="5">
+        <tr>
 
-	<?php
-	$db = mysqli_connect("127.0.0.1", "root", "Porsche1996");
-	mysqli_select_db($db,"librarie");
-	$sql1 = "Select * from carti";
-	$resursa1 = mysqli_query($db, $sql1);
-	$nr = mysqli_num_rows($resursa1);
-	print "<p>Sunt $nr carti in baza de date</p>";
-	?>
-
-	<h2>Domenii</h2>
-
-	<?php
-	/* si in continuare afisam numele de domenii: */
-	$sql2 = "select nume_domeniu FROM domenii";
-	$resursa2 = mysqli_query($db, $sql2);
-	while($row = mysqli_fetch_array($resursa2))
-	{
-		print '<a href="domeniu.php?nume_domeniu='.$row['nume_domeniu'].'">'.$row['nume_domeniu'].'</a></br>';
-	}
-	?>
-
-</body>
-</html>
+            <?php
+            $sql = "SELECT id_carte,titlu,nume_autor,pret FROM cartti,autori WHERE carti.id_autor=autori.id_autor ORDER BY data DESC LIMIT 0,3";
+            $resursa = mysqli_query($sql);
+            while($row = mysqli_fetch_array($resursa))
+            {
+                /*deschidem celula tabelului html*/
+                print '<td align="center">';
+                $adresaImagine = "coperte".$row['id_carte'].".jpg";
+                if(file_exists($adresaImagine))
+                {
+                    print '<img src="'.$adresaImagine.'" width="75" height="100"><br>';
+                }
+                else
+                    print '<b><a href="carte.php?id_carte='.$row['id_carte'].'">'.$row['titlu'].'</a></b><br> de <i>'.$row['nume_autor'].'</i><br> Pret: '.row['pret'].'lei</td>';
+            }
+            ?>
+        </tr>
+    </table>
+    <hr>
+    <b>Cele mai populare carti</b>
+    <table cellpadding="5">
+        <tr>
+<?php
+$sqlVanzari = "SELECT id_carte,sum(nr_buc) AS bucatiVandute FROM vanzari GROUP BY id_carte ORDER BY bucatiVandute DESC LIMIT 0,3";
+$resursaVanzari = mysqli_query($sqlVanzari);
+while($rowVanzari = mysqli_fetch_array($resursaVanzari))
+{
+    $sqlCarte = "SELECT titlu,nume_autor,pret FROM carti,autori WHERE carti.id_autor=autori.id_autor AND id_carte=".$rowVanzari['id_carte'];
+    $resursaCarte = mysqli_query($sqlCarte);
+}
+?>
---
+++ menu.php
@@ -0,0 +1,22 @@
+<td valign="top" width="125">
+    <div style="width:120px; background-color:#F9F1E7; padding:4px; border: solid #632415 1px">
+        <b>Alege domeniul</b><HR size="1">
+<?php
+$sql = "SELECT * FROM domenii ORDER BY nume_domeniu ASC";
+$resursa = mysqli_query($sql);
+while($row = mysqli_fetch_array($resursa))
+{
+    print 'a href="domeniu.php?id_domeniu=''.$row['id_domeniu'].'">'.$row['nume_domeniu'].'</a><br>';
+}
+
+?>
+
+</div>
+<br>
+<div style="width:120px; background-color:#F9F1E7; padding:4px; border:solid #632415 1px">
+	<form action="cautare.php" method="GET">
+	<b>Cautare</b></br>
+	<input type="submit" value="Cauta">
+	</form>
+</div>
+</td>
