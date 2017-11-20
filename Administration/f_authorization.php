3139181a20fb51579cf59efe9b7495fb017684b0
DianaAndreeaSimionca <33803484+DianaAndreeaSimionca@users.noreply.github.com>
2017-11-20 12:23:20 +0200

    modificare f_authorization.php

---
+++ Administration/f_authorization.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include ("../connect_db.php");
+
+function autorizat()
+{
+    $sql = "SELECT * FROM admin WHERE admin_nume='".$_SESSION['nume_admin']."' AND admin_parola='".$_SESSION['parola_encriptata']."'";
+    $resursa = mysqli_query($db, $sql);
+    if ($_SESSION['key_admin'] != session_id() || mysqli_num_rows($resursa) != 1)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+?>
