cf4587866a0351d30e40dd4e8e6b80b211e34793
DianaAndreeaSimionca <33803484+DianaAndreeaSimionca@users.noreply.github.com>
2017-11-20 02:17:47 +0200

    Modificare search.php

---
+++ search.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+include ("connect_db.php");
+include ("page_top.php");
+include ("menu.php");
+
+$cuvant = $_GET['cuvant'];
+?>
+
+<td valign="top">
+    <h1>Rezultatele cautarii</h1>
+    <p>Textul cautat: <b><?=$cuvant?></b></p>
+    <b>Autori</b>
+    <blackquote>
+        <?php
+        $sql = "SELECT id_autor, nume_autor FROM autori WHERE nume_autor LIKE '%".$cuvant."%'";
+        $resursa = mysqli_query($db, $sql);
+
+        if (mysqli_num_rows($resursa) == 0)
+        {
+            print "<i>Nici un rezultat</i>";
+        }
+        else
+            while ($row = mysqli_fetch_array($resursa))
+            {
+                print '<a href="autor.php?id_autor='.$row['id_autor'].'">'.$row['nume_autor'].'</a><br>';
+            }
+        ?>
+
+    </blackquote>
+    <b>Titluri</b>
+    <blackquote>
+
+    <?php
+    $sql = "SELECT id_carte, titlu FROM carti WHERE titlu LIKE '%".$cuvant."%'";
+    $resursa = mysqli_query($db, $sql);
+
+    if (mysqli_num_rows($resursa) == 0)
+    {
+        print "<i>Nici un rezultat</i>";
+    }
+    else
+        while ($row = mysqli_fetch_array($resursa))
+        {
+            print '<a href="carte.php?id_carte='.$row['id_carte'].'">'.$row['titlu'].'</a><br>'.$row['descriere'].'<br><br>';
+        }
+    ?>
+
+    </blackquote>
+</td>
+
+<?php
+include ("page_bottom.php");
+?>
