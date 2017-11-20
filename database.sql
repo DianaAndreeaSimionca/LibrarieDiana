c3fdc839b0e2435a5bd6dad7222d8a7f65c5eaad
NascaSergiu <sergiu1_nasca@yahoo.com>
2017-11-20 02:49:36 +0200

    update

--- database.sql
+++ database.sql
@@ -3,32 +3,29 @@ CREATE DATABASE librarie;
 USE librarie;
 
 CREATE TABLE carti (
-`id_carte` INT NOT NULL AUTO_INCREMENT ,
-`id_autor` INT NOT NULL ,
-`id_domeniu` INT NOT NULL ,
-`titlu` TEXT NOT NULL ,
-`descriere` TEXT NOT NULL ,
-`data` DATE NOT NULL ,
-`pret` DOUBLE NOT NULL ,
-PRIMARY KEY (`id_carte`), UNIQUE (`id_carte`), INDEX (`id_carte`)) ENGINE = InnoDB;
+  `id_carte` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `id_autor` INT NOT NULL ,
+  `id_domeniu` INT NOT NULL ,
+  `titlu` TEXT NOT NULL ,
+  `descriere` TEXT NOT NULL ,
+  `data` DATE NOT NULL ,
+  `pret` DOUBLE NOT NULL) ENGINE = InnoDB;
 
 INSERT INTO carti(id_autor, id_domeniu, titlu, descriere, data, pret) VALUES (1, 2, 'Luceafarul', 'Descriere Luceafarul', '2012-12-10', 30.5);
 INSERT INTO carti(id_autor, id_domeniu, titlu, descriere, data, pret) VALUES (2, 3, 'Morometii', 'Descriere Morometii', '2010-06-05', 50.0);
 INSERT INTO carti(id_autor, id_domeniu, titlu, descriere, data, pret) VALUES (4, 4, 'Romea si Julieta', 'Descriere Romea si Julieta', '2014-10-01', 20.0);
 
 CREATE TABLE domenii (
-`id_domeniu` INT NOT NULL AUTO_INCREMENT ,
-`nume_domeniu` TEXT NOT NULL ,
-PRIMARY KEY (`id_domeniu`), UNIQUE (`id_domeniu`), INDEX (`id_domeniu`)) ENGINE = InnoDB;
+  `id_domeniu` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `nume_domeniu` TEXT NOT NULL ) ENGINE = InnoDB;
 
 INSERT INTO domenii(nume_domeniu) VALUES ('Poezie');
 INSERT INTO domenii(nume_domeniu) VALUES ('Roman');
 INSERT INTO domenii(nume_domeniu) VALUES ('Proza');
 
 CREATE TABLE autori (
-`id_autor` INT NOT NULL AUTO_INCREMENT ,
-`nume_autor` TEXT NOT NULL ,
-PRIMARY KEY (`id_autor`), UNIQUE (`id_autor`), INDEX (`id_autor`)) ENGINE  = InnoDB;
+  `id_autor` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `nume_autor` TEXT NOT NULL ) ENGINE  = InnoDB;
 
 INSERT INTO autori(nume_autor) VALUES ('Mihai Eminescu');
 INSERT INTO autori(nume_autor) VALUES ('Marin Preda');
@@ -36,23 +33,20 @@ INSERT INTO autori(nume_autor) VALUES ('Tudor Arghezi');
 INSERT INTO autori(nume_autor) VALUES ('Wiliam Shakespear');
 
 CREATE TABLE comentarii(
-`id_comentariu` INT NOT NULL ,
-`nume_utilizator` TEXT NOT NULL ,
-`email_utilizator` TEXT NOT NULL ,
-`comentariu` TEXT NOT NULL ,
-`id_carte` INT NOT NULL ,
-PRIMARY KEY (`id_comentariu`), UNIQUE (`id_comentariu`), INDEX (`id_comentariu`)) ENGINE  = InnoDB;
+  `id_comentariu` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `nume_utilizator` TEXT NOT NULL ,
+  `email_utilizator` TEXT NOT NULL ,
+  `comentariu` TEXT NOT NULL ,
+  `id_carte` INT NOT NULL ) ENGINE  = InnoDB;
 
 CREATE TABLE tranzactii(
-`id_tranzactie` INT NOT NULL ,
-`data_tranzactie` TIMESTAMP(6) NOT NULL ,
-`nume_cumparator` TEXT NOT NULL ,
-`adresa_cumparator` TEXT NOT NULL ,
-PRIMARY KEY (`id_tranzactie`), UNIQUE (`id_tranzactie`), INDEX (`id_tranzactie`)) ENGINE  = InnoDB;
+  `id_tranzactie` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `data_tranzactie` TIMESTAMP(6) NOT NULL ,
+  `nume_cumparator` TEXT NOT NULL ,
+  `adresa_cumparator` TEXT NOT NULL ) ENGINE  = InnoDB;
 
 CREATE TABLE vanzari(
-`id_vanzare` INT NOT NULL ,
-`id_tranzactie` INT NOT NULL ,
-`id_carte` INT NOT NULL ,
-`nr_buc` INT NOT NULL ,
-PRIMARY KEY (`id_vanzare`), UNIQUE (`id_vanzare`), INDEX (`id_vanzare`)) ENGINE  = InnoDB;
+  `id_vanzare` INT NOT NULL AUTO_INCREMENT PRIMARY KEY UNIQUE ,
+  `id_tranzactie` INT NOT NULL ,
+  `id_carte` INT NOT NULL ,
+  `nr_buc` INT NOT NULL ) ENGINE  = InnoDB;
