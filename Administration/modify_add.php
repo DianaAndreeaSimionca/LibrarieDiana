26e76b28e4492eb8651fba10d5383467770eb7cc
DianaAndreeaSimionca <33803484+DianaAndreeaSimionca@users.noreply.github.com>
2017-11-20 15:58:40 +0200

    modificare modify_add.php

--- Administration/modify_add.php
+++ Administration/modify_add.php
@@ -51,4 +51,28 @@ if(isset($_POST['adauga_autor']))
     exit;
 }
 
+
+if (isset($_POST['adauga_carte']))
+{
+    if ($_POST['titlu'] == "" || $_POST['descriere'] == "" || $_POST['pret'] == "")
+    {
+        print 'Titlul, descrierea sau pretul sunt goale!<br><a href="add.php">Inapoi</a>';
+        exit;
+    }
+
+    if (!is_numeric($_POST['pret']))
+    {
+        print 'Campul Pret trebuie sa fie de tip numeric! (scrieti <b>1000</b> in loc de <b>1000 lei</b>)<br><a href="add.php">Inapoi</a>';
+        exit;
+    }
+
+    $sql = "INSERT INTO carti(id_domeniu, id_autor, titlu, descriere, pret) VALUES ('".$_POST['id_domeniu']."','".$_POST['id_autor']."','".$_POST['titlu']."','".$_POST['descriere']."','".$_POST['pret']."')";
+    mysqli_query($db, $sql);
+
+    print 'Cartea a fost adaugata in baza de date!<br><a href="add.php">Inapoi</a>';
+    exit;
+}
 ?>
+
+</body>
+</html>
